8f2e0e83ff1e98718d28c340237d3b4761733f3e
henvisi-1994 <henvisi1994@gmail.com>
2025-05-06 09:27:53 -0500

    rutas roles

--- src/infrestructure/server/auth/RolesService.ts
+++ src/infrestructure/server/auth/RolesService.ts
@@ -1,4 +1,48 @@
-const dataService:DataService<Usuario> = new DataService("login");
-export const RolController = {
-    
-}
+import { Request, Response } from "express";
+import { Rol } from "../../../core/modelo/auth/Rol";
+import { DataService } from "../DataService";
+
+const dataService: DataService<Rol> = new DataService("roles");
+export const RolesService = {
+  async obtener(req: Request, res: Response) {
+    try {
+      const where = { ...req.query }; // Convertir los parámetros de consulta en filtros
+      const rol = await dataService.getAll(["*"], where);
+      res.json(rol);
+    } catch (error) {
+      console.error("Error al obtener la rol:", error);
+      res.status(500).json({ message: "Error interno del servidor" });
+    }
+  },
+  guardar: async (req: Request, res: Response) => {
+    try {
+      const rol: Rol = req.body;
+      const savedrol = await dataService.processData(rol);
+      res.status(201).json(savedrol);
+    } catch (error) {
+      console.error("Error al guardar la rol:", error);
+      res.status(500).json({ message: "Error interno del servidor" });
+    }
+  },
+  async actualizar(req: Request, res: Response) {
+    try {
+      const id = parseInt(req.params.id);
+      const rol: Rol = req.body;
+      await dataService.updateById(id, rol);
+      res.status(200).json({ message: "rol actualizada correctamente" });
+    } catch (error) {
+      console.error("Error al actualizar la rol:", error);
+      res.status(500).json({ message: "Error interno del servidor" });
+    }
+  },
+  async eliminar(req: Request, res: Response) {
+    try {
+      const id = parseInt(req.params.id);
+      await dataService.deleteById(id);
+      res.status(200).json({ message: "rol eliminada correctamente" });
+    } catch (error) {
+      console.error("Error al eliminar la rol:", error);
+      res.status(500).json({ message: "Error interno del servidor" });
+    }
+  },
+};
--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,9 +1,15 @@
 import express from 'express';
 import { sessionAuth } from '../middleware/supabaseMidleware';
 import { AuthService } from '../infrestructure/server/auth/AuthService';
+import { RolesService } from '../infrestructure/server/auth/RolesService';
 const router = express.Router();
-
+const ruta_roles = '/roles';
 router.post('/', sessionAuth, AuthService.login);
 router.post('/', sessionAuth, AuthService.register);
 router.post('/', sessionAuth, AuthService.changePassword);
+//Roles
+router.get(ruta_roles+'/', sessionAuth, RolesService.obtener);
+router.post(ruta_roles+'/', sessionAuth, RolesService.guardar);
+router.put(ruta_roles+'/:id', sessionAuth, RolesService.actualizar);
+router.delete(ruta_roles+'/:id', sessionAuth, RolesService.eliminar);
 export default router;
