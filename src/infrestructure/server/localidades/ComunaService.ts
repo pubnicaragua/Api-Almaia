4d1c6d76dadb8a533716d07bccd41cd7d7a48236
henvisi-1994 <henvisi1994@gmail.com>
2025-05-06 16:08:44 -0500

    rutas comunas

---
+++ src/infrestructure/server/localidades/ComunaService.ts
@@ -0,0 +1,48 @@
+import { Request, Response } from "express";
+import { DataService } from "../DataService";
+import { Comuna } from "../../../core/modelo/localidades/Comuna";
+
+const dataService: DataService<Comuna> = new DataService("comunas");
+export const ComunaService = {
+  async obtener(req: Request, res: Response) {
+    try {
+      const where = { ...req.query }; // Convertir los parámetros de consulta en filtros
+      const comuna = await dataService.getAll(["*"], where);
+      res.json(comuna);
+    } catch (error) {
+      console.error("Error al obtener la comuna:", error);
+      res.status(500).json({ message: "Error interno del servidor" });
+    }
+  },
+  guardar: async (req: Request, res: Response) => {
+    try {
+      const comuna: Comuna = req.body;
+      const savedcomuna = await dataService.processData(comuna);
+      res.status(201).json(savedcomuna);
+    } catch (error) {
+      console.error("Error al guardar la comuna:", error);
+      res.status(500).json({ message: "Error interno del servidor" });
+    }
+  },
+  async actualizar(req: Request, res: Response) {
+    try {
+      const id = parseInt(req.params.id);
+      const comuna: Comuna = req.body;
+      await dataService.updateById(id, comuna);
+      res.status(200).json({ message: "comuna actualizada correctamente" });
+    } catch (error) {
+      console.error("Error al actualizar la comuna:", error);
+      res.status(500).json({ message: "Error interno del servidor" });
+    }
+  },
+  async eliminar(req: Request, res: Response) {
+    try {
+      const id = parseInt(req.params.id);
+      await dataService.deleteById(id);
+      res.status(200).json({ message: "comuna eliminada correctamente" });
+    } catch (error) {
+      console.error("Error al eliminar la comuna:", error);
+      res.status(500).json({ message: "Error interno del servidor" });
+    }
+  },
+};
--- src/routes/localidades.routes.ts
+++ src/routes/localidades.routes.ts
@@ -4,6 +4,7 @@ import { PaisService } from '../infrestructure/server/localidades/PaisService';
 const router = express.Router();
 const ruta_paises = '/paises';
 const ruta_regiones = '/regiones';
+const ruta_comunas = '/comunas';
 
 
 //Paises
@@ -17,6 +18,13 @@ router.get(ruta_regiones+'/', sessionAuth, PaisService.obtener);
 router.post(ruta_regiones+'/', sessionAuth, PaisService.guardar);    
 router.put(ruta_regiones+'/:id', sessionAuth, PaisService.actualizar);
 router.delete(ruta_regiones+'/:id', sessionAuth, PaisService.eliminar);
+
+//comunas
+router.get(ruta_comunas+'/', sessionAuth, PaisService.obtener);
+router.post(ruta_comunas+'/', sessionAuth, PaisService.guardar);
+router.put(ruta_comunas+'/:id', sessionAuth, PaisService.actualizar);
+router.delete(ruta_comunas+'/:id', sessionAuth, PaisService.eliminar);
+
 
 
 export default router;
