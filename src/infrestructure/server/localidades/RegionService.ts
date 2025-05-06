f681c5ffd54f2ede4db3b5b2ecad979c5f540e3c
henvisi-1994 <henvisi1994@gmail.com>
2025-05-06 16:02:05 -0500

    Rutas de regiones

---
+++ src/infrestructure/server/localidades/RegionService.ts
@@ -0,0 +1,48 @@
+import { Request, Response } from "express";
+import { DataService } from "../DataService";
+import { Region } from "../../../core/modelo/localidades/Region";
+
+const dataService: DataService<Region> = new DataService("regions");
+export const RegionService = {
+  async obtener(req: Request, res: Response) {
+    try {
+      const where = { ...req.query }; // Convertir los parámetros de consulta en filtros
+      const region = await dataService.getAll(["*"], where);
+      res.json(region);
+    } catch (error) {
+      console.error("Error al obtener la region:", error);
+      res.status(500).json({ message: "Error interno del servidor" });
+    }
+  },
+  guardar: async (req: Request, res: Response) => {
+    try {
+      const region: Region = req.body;
+      const savedregion = await dataService.processData(region);
+      res.status(201).json(savedregion);
+    } catch (error) {
+      console.error("Error al guardar la region:", error);
+      res.status(500).json({ message: "Error interno del servidor" });
+    }
+  },
+  async actualizar(req: Request, res: Response) {
+    try {
+      const id = parseInt(req.params.id);
+      const region: Region = req.body;
+      await dataService.updateById(id, region);
+      res.status(200).json({ message: "region actualizada correctamente" });
+    } catch (error) {
+      console.error("Error al actualizar la region:", error);
+      res.status(500).json({ message: "Error interno del servidor" });
+    }
+  },
+  async eliminar(req: Request, res: Response) {
+    try {
+      const id = parseInt(req.params.id);
+      await dataService.deleteById(id);
+      res.status(200).json({ message: "region eliminada correctamente" });
+    } catch (error) {
+      console.error("Error al eliminar la region:", error);
+      res.status(500).json({ message: "Error interno del servidor" });
+    }
+  },
+};
--- src/routes/localidades.routes.ts
+++ src/routes/localidades.routes.ts
@@ -3,13 +3,20 @@ import { sessionAuth } from '../middleware/supabaseMidleware';
 import { PaisService } from '../infrestructure/server/localidades/PaisService';
 const router = express.Router();
 const ruta_paises = '/paises';
+const ruta_regiones = '/regiones';
 
 
-//Avisos
+//Paises
 router.get(ruta_paises+'/', sessionAuth, PaisService.obtener);
 router.post(ruta_paises+'/', sessionAuth, PaisService.guardar);
 router.put(ruta_paises+'/:id', sessionAuth, PaisService.actualizar);
 router.delete(ruta_paises+'/:id', sessionAuth, PaisService.eliminar);
+
+// regiones
+router.get(ruta_regiones+'/', sessionAuth, PaisService.obtener);
+router.post(ruta_regiones+'/', sessionAuth, PaisService.guardar);    
+router.put(ruta_regiones+'/:id', sessionAuth, PaisService.actualizar);
+router.delete(ruta_regiones+'/:id', sessionAuth, PaisService.eliminar);
 
 
 export default router;
