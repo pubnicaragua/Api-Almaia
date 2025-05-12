294d5bf5190a6ee2ccf1413b9f3e16c61768174f
henvisi-1994 <henvisi1994@gmail.com>
2025-05-11 22:52:55 -0500

    CRUD Curso

--- src/infrestructure/server/colegio/CursoService.ts
+++ src/infrestructure/server/colegio/CursoService.ts
@@ -1,84 +1,135 @@
 import { Request, Response } from "express";
 import { DataService } from "../DataService";
 import { Curso } from "../../../core/modelo/colegio/Curso";
+import { SupabaseClientService } from "../../../core/services/supabaseClient";
+import { SupabaseClient } from "@supabase/supabase-js";
+import Joi from "joi";
 
-const dataService: DataService<Curso> = new DataService(
-  "cursos"
-);
+const supabaseService = new SupabaseClientService();
+const client: SupabaseClient = supabaseService.getClient();
+const CursoSchema = Joi.object({
+  nombre_curso: Joi.string().max(50).required(),
+  colegio_id: Joi.number().integer().required(),
+  grado_id: Joi.number().integer().required(),
+  nivel_id: Joi.number().integer().required(),
+});
+const dataService: DataService<Curso> = new DataService("cursos");
 export const CursosService = {
-    async obtener(req: Request, res: Response) {
-        try {
-        /*const calendarios = await dataService.getAll(["*"], req.query);
-                res.json(calendarios);*/
-        const cursos = [
-            {
-            curso_id: 1,
-            colegio_id: 1,
-            nombre: "Primero Básico",
-            nivel: "Básico",
-            seccion: "A",
-            anio_escolar: 2025,
-            },
-            {
-            curso_id: 2,
-            colegio_id: 1,
-            nombre: "Segundo Básico",
-            nivel: "Básico",
-            seccion: "B",
-            anio_escolar: 2025,
-            },
-            {
-            curso_id: 3,
-            colegio_id: 2,
-            nombre: "Tercero Medio",
-            nivel: "Medio",
-            seccion: "C",
-            anio_escolar: 2025,
-            },
-            {
-            curso_id: 4,
-            colegio_id: 3,
-            nombre: "Cuarto Medio",
-            nivel: "Medio",
-            seccion: "D",
-            anio_escolar: 2025,
-            },
-        ];
-        res.json(cursos);
-        } catch (error) {
-        res.status(500).json(error);
-        }
-    },
+  async obtener(req: Request, res: Response) {
+    try {
+      const cursos = await dataService.getAll(
+        [
+          "*",
+          "colegios(colegio_id,nombre)",
+          "grados(grado_id,nombre)",
+          "niveles_educativos(nivel_educativo_id,nombre)",
+        ],
+        req.query
+      );
+      res.json(cursos);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  },
 
-    async guardar(req: Request, res: Response) {
-        try {
-            const nuevoCurso = req.body;
-            const cursoCreado = await dataService.processData(nuevoCurso);
-            res.status(201).json(cursoCreado);
-        } catch (error) {
-            res.status(500).json(error);
-        }
-    },
-    async actualizar(req: Request, res: Response) {
-        try {
-            const cursoId = parseInt(req.params.id);
-            const cursoActualizado = req.body;
-            const resultado = await dataService.updateById(cursoId, cursoActualizado);
-            res.status(200).json(resultado);
-           
-        } catch (error) {
-            res.status(500).json(error);
-        }
-    },
-    async eliminar(req: Request, res: Response) {
-        try {
-            const cursoId = parseInt(req.params.id);
-            await dataService.deleteById(cursoId);
-            res.status(200).json({ message: "Curso eliminado" });
-        }
-        catch (error) {
-            res.status(500).json(error);
-        }
-    }    
+  async guardar(req: Request, res: Response) {
+    try {
+      const curso = new Curso();
+      Object.assign(curso, req.body);
+      curso.creado_por = req.creado_por;
+      curso.actualizado_por = req.actualizado_por;
+      let responseSent = false;
+      const { error: validationError } = CursoSchema.validate(req.body);
+      const { data, error } = await client
+        .from("colegios")
+        .select("*")
+        .eq("colegio_id", curso.colegio_id)
+        .single();
+      if (error || !data) {
+        throw new Error("El colegio no existe");
+      }
+      const { data: dataGrado, error: errorGrado } = await client
+        .from("grados")
+        .select("*")
+        .eq("grado_id", curso.grado_id)
+        .single();
+      if (errorGrado || !dataGrado) {
+        throw new Error("El grado no existe");
+      }
+      const { data: dataNivelEducativo, error: errorNivelEducativo } =
+        await client
+          .from("niveles_educativos")
+          .select("*")
+          .eq("nivel_educativo_id", curso.nivel_educativo_id)
+          .single();
+      if (errorNivelEducativo || !dataNivelEducativo) {
+        throw new Error("El nivel educativo no existe");
+      }
+      if (validationError) {
+        responseSent = true;
+        throw new Error(validationError.details[0].message);
+      }
+      if (!responseSent) {
+        const cursoCreado = await dataService.processData(curso);
+        res.status(201).json(cursoCreado);
+      }
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  },
+  async actualizar(req: Request, res: Response) {
+    try {
+      const cursoId = parseInt(req.params.id);
 
-}
+      const curso = new Curso();
+      Object.assign(curso, req.body);
+      curso.actualizado_por = req.actualizado_por;
+      let responseSent = false;
+      const { error: validationError } = CursoSchema.validate(req.body);
+      const { data, error } = await client
+        .from("colegios")
+        .select("*")
+        .eq("colegio_id", curso.colegio_id)
+        .single();
+      if (error || !data) {
+        throw new Error("El colegio no existe");
+      }
+      const { data: dataGrado, error: errorGrado } = await client
+        .from("grados")
+        .select("*")
+        .eq("grado_id", curso.grado_id)
+        .single();
+      if (errorGrado || !dataGrado) {
+        throw new Error("El grado no existe");
+      }
+      const { data: dataNivelEducativo, error: errorNivelEducativo } =
+        await client
+          .from("niveles_educativos")
+          .select("*")
+          .eq("nivel_educativo_id", curso.nivel_educativo_id)
+          .single();
+      if (errorNivelEducativo || !dataNivelEducativo) {
+        throw new Error("El nivel educativo no existe");
+      }
+      if (validationError) {
+        responseSent = true;
+        throw new Error(validationError.details[0].message);
+      }
+      if (!responseSent) {
+        const resultado = await dataService.updateById(cursoId, curso);
+        res.status(200).json(resultado);
+      }
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  },
+  async eliminar(req: Request, res: Response) {
+    try {
+      const cursoId = parseInt(req.params.id);
+      await dataService.deleteById(cursoId);
+      res.status(200).json({ message: "Curso eliminado" });
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  },
+};
