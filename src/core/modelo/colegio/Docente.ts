647b82431657849fa06f7150c8a6ad1c30b5a506
henvisi-1994 <henvisi1994@gmail.com>
2025-05-15 09:13:22 -0500

    CRUD Docente

--- src/core/modelo/colegio/Docente.ts
+++ src/core/modelo/colegio/Docente.ts
@@ -1,14 +1,13 @@
 import { BaseModel } from "../BaseModel";
 
 export class Docente extends BaseModel {
-    docente_id: number;
+    docente_id?: number;
     persona_id: number;
     colegio_id: number;
     especialidad: string;
     estado: string;
     constructor(){
         super();
-        this.docente_id = 0;
         this.persona_id = 0;
         this.colegio_id = 0;
         this.especialidad = "";
--- src/infrestructure/server/docente/DocenteService.ts
+++ src/infrestructure/server/docente/DocenteService.ts
@@ -1,8 +1,19 @@
 import { Request, Response } from "express";
 import { DataService } from "../DataService";
 import { Docente } from "../../../core/modelo/colegio/Docente";
+import { SupabaseClientService } from "../../../core/services/supabaseClient";
+import { SupabaseClient } from "@supabase/supabase-js";
+import Joi from "joi";
 
-const dataService: DataService<Docente> = new DataService("docentes");
+const supabaseService = new SupabaseClientService();
+const client: SupabaseClient = supabaseService.getClient();
+const dataService: DataService<Docente> = new DataService("docentes","docente_id");
+const DocenteSchema = Joi.object({
+  persona_id: Joi.number().integer().required(),
+  colegio_id: Joi.number().integer().required(),
+  especialidad: Joi.string().max(100).required(),
+  estado: Joi.string().max(20).required(),
+});
 export const DocentesService = {
   async obtener(req: Request, res: Response) {
     try {
@@ -23,44 +34,17 @@ export const DocentesService = {
   },
   async detalle(req: Request, res: Response) {
     try {
-      //const id = parseInt(req.params.id);
-      //const where = { docente_id: id }; // Convertir los parámetros de consulta en filtros
-      /*const docente = await dataService.getAll(["*",
-        "personas(persona_id,nombres,apellidos)",
-        "colegios(colegio_id,nombre)"], where);*/
-      const docente = {
-        docente_id: 1,
-        persona_id: 1,
-        colegio_id: 1,
-        especialidad: "Matemáticas",
-        estado: "Activo",
-        persona: {
-          persona_id: 1,
-          nombres: "Juan",
-          apellidos: "Pérez",
-        },
-        colegio: {
-          colegio_id: 1,
-          nombre: "Colegio San Juan",
-        },
-        docentes_cursos: {
-          docente_curso_id: 1,
-          cursos: [
-            {
-              curso_id: 1,
-              nombre_curso: "matematicas",
-              grados: {
-                grado_id: 1,
-                nombre: "4to",
-              },
-              niveles_educativos: {
-                nivel_educativo: 1,
-                nombre: "basico",
-              },
-            },
-          ],
-        },
-      };
+      const id = parseInt(req.params.id);
+      const where = { docente_id: id }; // Convertir los parámetros de consulta en filtros
+      const docente_data = await dataService.getAll(
+        [
+          "*",
+          "personas(persona_id,nombres,apellidos)",
+          "colegios(colegio_id,nombre)",
+        ],
+        where
+      );
+      const docente = docente_data[0];
       res.json(docente);
     } catch (error) {
       console.error("Error al obtener el docente:", error);
@@ -69,20 +53,77 @@ export const DocentesService = {
   },
   guardar: async (req: Request, res: Response) => {
     try {
-      const docente: Docente = req.body;
-      const savedocente = await dataService.processData(docente);
-      res.status(201).json(savedocente);
-    } catch (error) {
+      const docente: Docente = new Docente();
+      Object.assign(docente, req.body);
+      docente.creado_por = req.creado_por;
+      docente.actualizado_por = req.actualizado_por;
+      let responseSent = false;
+      const { error: validationError } = DocenteSchema.validate(req.body);
+      const { data, error } = await client
+        .from("personas")
+        .select("*")
+        .eq("persona_id", docente.persona_id)
+        .single();
+      if (error || !data) {
+        throw new Error("La persona no existe");
+      }
+      const { data: dataColegio, error: errorColegio } = await client
+        .from("colegios")
+        .select("*")
+        .eq("colegio_id", docente.colegio_id)
+        .single();
+      if (errorColegio || !dataColegio) {
+        throw new Error("El colegio no existe");
+      }
+
+      if (validationError) {
+        responseSent = true;
+        throw new Error(validationError.details[0].message);
+      }
+      if (!responseSent) {
+        const savedDocente = await dataService.processData(docente);
+        res.status(201).json(savedDocente);
+      }
+    } catch (err) {
+      const error = err as Error;
       console.error("Error al guardar el docente:", error);
-      res.status(500).json({ message: "Error interno del servidor" });
+      res.status(500).json({ message: error.message || "Error inesperado" });
     }
   },
   async actualizar(req: Request, res: Response) {
     try {
       const id = parseInt(req.params.id);
-      const docente: Docente = req.body;
-      await dataService.updateById(id, docente);
-      res.status(200).json({ message: "docente actualizado correctamente" });
+      const docente: Docente = new Docente();
+      Object.assign(docente, req.body);
+      docente.creado_por = req.creado_por;
+      docente.actualizado_por = req.actualizado_por;
+      let responseSent = false;
+      const { error: validationError } = DocenteSchema.validate(req.body);
+      const { data, error } = await client
+        .from("personas")
+        .select("*")
+        .eq("persona_id", docente.persona_id)
+        .single();
+      if (error || !data) {
+        throw new Error("La persona no existe");
+      }
+      const { data: dataColegio, error: errorColegio } = await client
+        .from("colegios")
+        .select("*")
+        .eq("colegio_id", docente.colegio_id)
+        .single();
+      if (errorColegio || !dataColegio) {
+        throw new Error("El colegio no existe");
+      }
+
+      if (validationError) {
+        responseSent = true;
+        throw new Error(validationError.details[0].message);
+      }
+      if (!responseSent) {
+        await dataService.updateById(id, docente);
+        res.status(200).json({ message: "docente actualizado correctamente" });
+      }
     } catch (error) {
       console.error("Error al actualizar el docente:", error);
       res.status(500).json({ message: "Error interno del servidor" });
