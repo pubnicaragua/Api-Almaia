26615511a2cfa73a9052bad960cf9bf34956581b
henvisi-1994 <henvisi1994@gmail.com>
2025-05-15 17:47:22 -0500

    Crud de respuesta Posible

--- src/core/modelo/preguntasRespuestas/RespuestaPosible.ts
+++ src/core/modelo/preguntasRespuestas/RespuestaPosible.ts
@@ -1,11 +1,10 @@
 import { BaseModel } from "../BaseModel";
 
 export class RespuestaPosible extends BaseModel {
-    respuesta_posible_id: number;
+    respuesta_posible_id?: number;
     nombre: string;
     constructor(){
         super();
-        this.respuesta_posible_id = 0;
         this.nombre = "";
     }
   }
--- src/infrestructure/server/preguntas/RespuestaPosibleService.ts
+++ src/infrestructure/server/preguntas/RespuestaPosibleService.ts
@@ -1,55 +1,87 @@
 import { Request, Response } from "express";
 import { DataService } from "../DataService";
 import { RespuestaPosible } from "../../../core/modelo/preguntasRespuestas/RespuestaPosible";
+import Joi from "joi";
 
-const dataService:DataService<RespuestaPosible> = new DataService("respuestaposibles");
+const dataService: DataService<RespuestaPosible> = new DataService(
+  "respuestas_posibles", "respuesta_posible_id"
+);
+const RespuestaPosibleSchema = Joi.object({
+  nombre: Joi.string().max(50).required(),
+});
 export const RespuestaPosibleService = {
-    async obtener(req: Request, res: Response) {
-        try {
-           /* const where = { ...req.query }; // Convertir los parámetros de consulta en filtros
-            const respuestaposible = await dataService.getAll(["*"],where);
-            res.json(respuestaposible);*/
-            const respuestas = [
-                {
-                  "respuesta_posible_id": 1,
-                  "nombre": "Siempre"
-                }
-              ]
-            res.json(respuestas);
-        } catch (error) {
-            console.error("Error al obtener la respuestaposible:", error);
-             res.status(500).json({ message: "Error interno del servidor" });
-        }
-    },
-    guardar: async (req: Request, res: Response) => {
-        try {
-            const respuestaposible: RespuestaPosible = req.body;
-            const savedRespuestaPosible = await dataService.processData(respuestaposible);
-            res.status(201).json(savedRespuestaPosible);
-        } catch (error) {
-            console.error("Error al guardar la respuestaposible:", error);
-             res.status(500).json({ message: "Error interno del servidor" });
-        }
-    },
-    async actualizar(req: Request, res: Response) {
-        try {
-            const id = parseInt(req.params.id);
-            const respuestaposible: RespuestaPosible = req.body;
-            await dataService.updateById(id, respuestaposible);
-            res.status(200).json({ message: "RespuestaPosible actualizada correctamente" });
-        } catch (error) {
-            console.error("Error al actualizar la respuestaposible:", error);
-             res.status(500).json({ message: "Error interno del servidor" });
-        }
-    },
-    async eliminar(req: Request, res: Response) {
-        try {
-            const id = parseInt(req.params.id);
-            await dataService.deleteById(id);
-            res.status(200).json({ message: "RespuestaPosible eliminada correctamente" });
-        } catch (error) {
-            console.error("Error al eliminar la respuestaposible:", error);
-             res.status(500).json({ message: "Error interno del servidor" });
-        }
+  async obtener(req: Request, res: Response) {
+    try {
+      const where = { ...req.query }; // Convertir los parámetros de consulta en filtros
+      const respuestas = await dataService.getAll(["*"], where);
+      res.json(respuestas);
+    } catch (error) {
+      console.error("Error al obtener la respuestaposible:", error);
+      res.status(500).json({ message: "Error interno del servidor" });
     }
-}
+  },
+  guardar: async (req: Request, res: Response) => {
+    try {
+      const respuestaposible: RespuestaPosible = new RespuestaPosible();
+      Object.assign(respuestaposible, req.body);
+      respuestaposible.creado_por = req.creado_por;
+      respuestaposible.actualizado_por = req.actualizado_por;
+      let responseSent = false;
+      const { error: validationError } = RespuestaPosibleSchema.validate(
+        req.body
+      );
+      if (validationError) {
+        responseSent = true;
+        throw new Error(validationError.details[0].message);
+      }
+      if (!responseSent) {
+        const savedRespuestaPosible = await dataService.processData(
+          respuestaposible
+        );
+        res.status(201).json(savedRespuestaPosible);
+      }
+    } catch (err) {
+      const error = err as Error;
+      console.error("Error al guardar el respuestaposible:", error);
+      res.status(500).json({ message: error.message || "Error inesperado" });
+    }
+  },
+  async actualizar(req: Request, res: Response) {
+    try {
+      const id = parseInt(req.params.id);
+      const respuestaposible: RespuestaPosible = new RespuestaPosible();
+      Object.assign(respuestaposible, req.body);
+      respuestaposible.creado_por = req.creado_por;
+      respuestaposible.actualizado_por = req.actualizado_por;
+      let responseSent = false;
+      const { error: validationError } = RespuestaPosibleSchema.validate(
+        req.body
+      );
+      if (validationError) {
+        responseSent = true;
+        throw new Error(validationError.details[0].message);
+      }
+      if (!responseSent) {
+        await dataService.updateById(id, respuestaposible);
+        res
+          .status(200)
+          .json({ message: "RespuestaPosible actualizada correctamente" });
+      }
+    } catch (error) {
+      console.error("Error al actualizar la respuestaposible:", error);
+      res.status(500).json({ message: "Error interno del servidor" });
+    }
+  },
+  async eliminar(req: Request, res: Response) {
+    try {
+      const id = parseInt(req.params.id);
+      await dataService.deleteById(id);
+      res
+        .status(200)
+        .json({ message: "RespuestaPosible eliminada correctamente" });
+    } catch (error) {
+      console.error("Error al eliminar la respuestaposible:", error);
+      res.status(500).json({ message: "Error interno del servidor" });
+    }
+  },
+};
