84a1ef9302760f13403c0e77ec24f001d58589a2
henvisi-1994 <henvisi1994@gmail.com>
2025-05-15 23:24:17 -0500

    Crud de preguntas

--- src/core/modelo/preguntasRespuestas/Pregunta.ts
+++ src/core/modelo/preguntasRespuestas/Pregunta.ts
@@ -1,11 +1,10 @@
 import { BaseModel } from "../BaseModel";
 
 export class Pregunta extends BaseModel {
-    pregunta_id: number;
+    pregunta_id?: number;
     tipo_pregunta_id: number;
     nivel_educativo_id: number;
     diagnostico: string;
-    tipo_respuesta: string;
     sintomas: string;
     grupo_preguntas: string;
     palabra_clave: string;
@@ -13,11 +12,9 @@ export class Pregunta extends BaseModel {
     texto_pregunta: string;
     constructor(){
         super();
-        this.pregunta_id = 0;
         this.tipo_pregunta_id = 0;
         this.nivel_educativo_id = 0;
         this.diagnostico = "";
-        this.tipo_respuesta = "";
         this.sintomas = "";
         this.grupo_preguntas = "";
         this.palabra_clave = "";
--- src/infrestructure/server/preguntas/PreguntaService.ts
+++ src/infrestructure/server/preguntas/PreguntaService.ts
@@ -1,35 +1,34 @@
 import { Request, Response } from "express";
 import { Pregunta } from "../../../core/modelo/preguntasRespuestas/Pregunta";
 import { DataService } from "../DataService";
-
+import { SupabaseClientService } from "../../../core/services/supabaseClient";
+import { SupabaseClient } from "@supabase/supabase-js";
+import Joi from "joi";
+const supabaseService = new SupabaseClientService();
+const client: SupabaseClient = supabaseService.getClient();
+const PreguntaSchema = Joi.object({
+  tipo_pregunta_id: Joi.number().integer().required(),
+  nivel_educativo_id: Joi.number().integer().required(),
+  diagnostico: Joi.string().required(),
+  sintomas: Joi.string().max(50).required(),
+  grupo_preguntas: Joi.string().max(10).required(),
+  palabra_clave: Joi.string().max(10).required(),
+  horario: Joi.string().max(10).required(),
+  texto_pregunta: Joi.string().required(),
+});
 const dataService: DataService<Pregunta> = new DataService("preguntas");
 export const PreguntaService = {
   async obtener(req: Request, res: Response) {
     try {
-      /* const where = { ...req.query }; // Convertir los parámetros de consulta en filtros
-            const pregunta = await dataService.getAll(["*"],where);
-            res.json(pregunta);*/
-      const preguntas = [
-        {
-          pregunta_id: 1,
-          tipo_pregunta: {
-            tipo_pregunta_id: 2,
-            nombre: "Opción múltiple",
-          },
-          nivel_educativo: {
-            nivel_educativo_id: 3,
-            nombre: "Secundaria",
-          },
-          diagnostico: "Problemas de atención",
-          sintomas: "Falta de concentración, hiperactividad",
-          grupo_preguntas: "Evaluación inicial",
-          palabra_clave: "atención",
-          horario: "am",
-          texto_pregunta:
-            "¿Con qué frecuencia tiene dificultad para concentrarse en sus tareas?",
-        },
-      ];
-
+      const where = { ...req.query }; // Convertir los parámetros de consulta en filtros
+      const preguntas = await dataService.getAll(
+        [
+          "*",
+          "tipos_preguntas(tipo_pregunta_id,nombre)",
+          "niveles_educativos(nivel_educativo_id,nombre)",
+        ],
+        where
+      );
       res.json(preguntas);
     } catch (error) {
       console.error("Error al obtener la pregunta:", error);
@@ -38,45 +37,18 @@ export const PreguntaService = {
   },
   async detalle(req: Request, res: Response) {
     try {
-      /* const where = { ...req.query }; // Convertir los parámetros de consulta en filtros
-            const pregunta = await dataService.getAll(["*"],where);
-            res.json(pregunta);*/
-      const pregunta = {
-        pregunta_id: 1,
-        tipo_pregunta: {
-          tipo_pregunta_id: 2,
-          nombre: "Opción múltiple",
-        },
-        nivel_educativo: {
-          nivel_educativo_id: 3,
-          nombre: "Secundaria",
-        },
-        diagnostico: "Problemas de atención",
-        sintomas: "Falta de concentración, hiperactividad",
-        grupo_preguntas: "Evaluación inicial",
-        palabra_clave: "atención",
-        horario: "am",
-        texto_pregunta:
-          "¿Con qué frecuencia tiene dificultad para concentrarse en sus tareas?",
-        respuestas_posibles_has_preguntas: [
-          {
-            respuesta_posible_id: 1,
-            pregunta_id: 1,
-            nombre: "Nunca",
-          },
-          {
-            respuesta_posible_id: 2,
-            pregunta_id: 1,
-            nombre: "Ocasionalmente",
-          },
-          {
-            respuesta_posible_id: 3,
-            pregunta_id: 1,
-            nombre: "Frecuentemente",
-          },
+      const id = parseInt(req.params.id);
+      const where = { pregunta_id: id }; // Convertir los parámetros de consulta en filtros
+      const pregunta_data = await dataService.getAll(
+        [
+          "*",
+          "tipos_preguntas(tipo_pregunta_id,nombre)",
+          "niveles_educativos(nivel_educativo_id,nombre)",
         ],
-      };
-
+        where
+      );
+      const pregunta = pregunta_data[0];
+      res.json(pregunta);
       res.json(pregunta);
     } catch (error) {
       console.error("Error al obtener la pregunta:", error);
@@ -85,20 +57,77 @@ export const PreguntaService = {
   },
   guardar: async (req: Request, res: Response) => {
     try {
-      const pregunta: Pregunta = req.body;
-      const savedPregunta = await dataService.processData(pregunta);
-      res.status(201).json(savedPregunta);
+      const pregunta: Pregunta = new Pregunta();
+      Object.assign(pregunta, req.body);
+      pregunta.creado_por = req.creado_por;
+      pregunta.actualizado_por = req.actualizado_por;
+      let responseSent = false;
+      const { error: validationError } = PreguntaSchema.validate(req.body);
+      const { data, error } = await client
+        .from("tipos_preguntas")
+        .select("*")
+        .eq("tipo_pregunta_id", pregunta.tipo_pregunta_id)
+        .single();
+      if (error || !data) {
+        throw new Error("El colegio no existe");
+      }
+      const { data: dataNivelesEducativos, error: errorNilesEducativos } =
+        await client
+          .from("niveles_educativos")
+          .select("*")
+          .eq("nivel_educativo_id", pregunta.nivel_educativo_id)
+          .single();
+      if (errorNilesEducativos || !dataNivelesEducativos) {
+        throw new Error("El colegio no existe");
+      }
+      if (validationError) {
+        responseSent = true;
+        throw new Error(validationError.details[0].message);
+      }
+      if (!responseSent) {
+        const savedPregunta = await dataService.processData(pregunta);
+        res.status(201).json(savedPregunta);
+      }
     } catch (error) {
-      console.error("Error al guardar la pregunta:", error);
+      console.error("Error al guardar el pregunta:", error);
       res.status(500).json({ message: "Error interno del servidor" });
     }
   },
   async actualizar(req: Request, res: Response) {
     try {
       const id = parseInt(req.params.id);
+
       const pregunta: Pregunta = req.body;
-      await dataService.updateById(id, pregunta);
-      res.status(200).json({ message: "Pregunta actualizada correctamente" });
+      Object.assign(pregunta, req.body);
+      pregunta.creado_por = req.creado_por;
+      pregunta.actualizado_por = req.actualizado_por;
+      let responseSent = false;
+      const { error: validationError } = PreguntaSchema.validate(req.body);
+      const { data, error } = await client
+        .from("tipos_preguntas")
+        .select("*")
+        .eq("tipo_pregunta_id", pregunta.tipo_pregunta_id)
+        .single();
+      if (error || !data) {
+        throw new Error("El colegio no existe");
+      }
+      const { data: dataNivelesEducativos, error: errorNilesEducativos } =
+        await client
+          .from("niveles_educativos")
+          .select("*")
+          .eq("nivel_educativo_id", pregunta.nivel_educativo_id)
+          .single();
+      if (errorNilesEducativos || !dataNivelesEducativos) {
+        throw new Error("El colegio no existe");
+      }
+      if (validationError) {
+        responseSent = true;
+        throw new Error(validationError.details[0].message);
+      }
+      if (!responseSent) {
+        await dataService.updateById(id, pregunta);
+        res.status(200).json({ message: "Pregunta actualizada correctamente" });
+      }
     } catch (error) {
       console.error("Error al actualizar la pregunta:", error);
       res.status(500).json({ message: "Error interno del servidor" });
