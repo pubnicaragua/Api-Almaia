a9b0e374eee467b54b8096ebede849d82b635253
henvisi-1994 <henvisi1994@gmail.com>
2025-05-21 16:28:43 -0500

    filtro colegio auditoria permiso

--- src/infrestructure/server/auth/AuditoriaPermisoService.ts
+++ src/infrestructure/server/auth/AuditoriaPermisoService.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import { DataService } from "../DataService";
 import Joi from "joi";
 import { AuditoriaPermiso } from "../../../core/modelo/configuracion/AuditoriaPermiso";
+import { obtenerRelacionados } from "../../../core/services/ObtenerTablasColegioCasoUso";
 const AuditoriaPermisoSchema = Joi.object({
   auditoria_id: Joi.number().integer().required(),
   nombre_permiso: Joi.string().max(50).required(),
@@ -13,9 +14,32 @@ const dataService: DataService<AuditoriaPermiso> = new DataService(
 export const AuditoriaPermisoesService = {
   async obtener(req: Request, res: Response) {
     try {
-      const where = { ...req.query }; // Convertir los parámetros de consulta en filtros
-      const auditoriapermiso = await dataService.getAll(["*"], where);
-      res.json(auditoriapermiso);
+      const { colegio_id, ...where } = req.query;
+      let respuestaEnviada = false;
+      if (colegio_id !== undefined) {
+        const auditoriapermiso = await obtenerRelacionados({
+          tableFilter: "auditorias",
+          filterField: "colegio_id",
+          filterValue: colegio_id,
+          idField: "auditoria_id",
+          tableIn: "auditorias_permisos",
+          inField: "auditoria_id",
+          selectFields: `*,                      
+                        auditorias(auditoria_id,fecha,usuario_id,descripcion,modulo_afectado,accion_realizada,ip_origen,referencia_id,model)",`,
+        });
+        respuestaEnviada = true;
+        res.json(auditoriapermiso);
+      }
+      if (!respuestaEnviada) {
+        const auditoriapermiso = await dataService.getAll(
+          [
+            "*",
+            "auditorias(auditoria_id,fecha,usuario_id,descripcion,modulo_afectado,accion_realizada,ip_origen,referencia_id,model)",
+          ],
+          where
+        );
+        res.json(auditoriapermiso);
+      }
     } catch (error) {
       console.error("Error al obtener la auditoriapermiso:", error);
       res.status(500).json({ message: "Error interno del servidor" });
@@ -26,13 +50,17 @@ export const AuditoriaPermisoesService = {
       const auditoriapermiso: AuditoriaPermiso = new AuditoriaPermiso();
       Object.assign(auditoriapermiso, req.body);
       let responseSent = false;
-      const { error: validationError } = AuditoriaPermisoSchema.validate(req.body);
+      const { error: validationError } = AuditoriaPermisoSchema.validate(
+        req.body
+      );
       if (validationError) {
         responseSent = true;
         throw new Error(validationError.details[0].message);
       }
       if (!responseSent) {
-        const savedauditoriapermiso = await dataService.processData(auditoriapermiso);
+        const savedauditoriapermiso = await dataService.processData(
+          auditoriapermiso
+        );
         res.status(201).json(savedauditoriapermiso);
       }
     } catch (error) {
@@ -46,7 +74,9 @@ export const AuditoriaPermisoesService = {
       const auditoriapermiso: AuditoriaPermiso = new AuditoriaPermiso();
       Object.assign(auditoriapermiso, req.body);
       let responseSent = false;
-      const { error: validationError } = AuditoriaPermisoSchema.validate(req.body);
+      const { error: validationError } = AuditoriaPermisoSchema.validate(
+        req.body
+      );
       if (validationError) {
         responseSent = true;
         throw new Error(validationError.details[0].message);
@@ -66,7 +96,9 @@ export const AuditoriaPermisoesService = {
     try {
       const id = parseInt(req.params.id);
       await dataService.deleteById(id);
-      res.status(200).json({ message: "auditoriapermiso eliminada correctamente" });
+      res
+        .status(200)
+        .json({ message: "auditoriapermiso eliminada correctamente" });
     } catch (error) {
       console.error("Error al eliminar la auditoriapermiso:", error);
       res.status(500).json({ message: "Error interno del servidor" });
