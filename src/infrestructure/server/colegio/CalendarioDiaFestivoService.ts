aca8b8a8726ff04af79003b9cc5818b9ab485cff
henvisi-1994 <henvisi1994@gmail.com>
2025-05-21 16:10:02 -0500

    filtro colegios en calendario dias festivos

--- src/infrestructure/server/colegio/CalendarioDiaFestivoService.ts
+++ src/infrestructure/server/colegio/CalendarioDiaFestivoService.ts
@@ -1,78 +1,72 @@
 import { Request, Response } from "express";
 import { DataService } from "../DataService";
 import { CalendarioDiaFestivo } from "../../../core/modelo/colegio/CalendarioDiaFestivo";
+import { obtenerRelacionados } from "../../../core/services/ObtenerTablasColegioCasoUso";
 
-const dataService: DataService<CalendarioDiaFestivo> = new DataService("calendariodiafestivos");
+const dataService: DataService<CalendarioDiaFestivo> = new DataService(
+  "calendarios_dias_festivos",
+  "calendario_dia_festivo_id"
+);
 export const CalendarioDiaFestivosService = {
-    async obtener(req: Request, res: Response) {
-        try {
-           /* const calendarios = await dataService.getAll(["*"], req.query);
-            res.json(calendarios);*/
-            const diasFestivos = [
-                {
-                  calendario_dia_festivo: 1,
-                  calendario_escolar_id: 1,
-                  dia_festivo: new Date("2025-01-01"),
-                  descripcion: "Año Nuevo"
-                },
-                {
-                  calendario_dia_festivo: 2,
-                  calendario_escolar_id: 1,
-                  dia_festivo: new Date("2025-04-18"),
-                  descripcion: "Viernes Santo"
-                },
-                {
-                  calendario_dia_festivo: 3,
-                  calendario_escolar_id: 1,
-                  dia_festivo: new Date("2025-05-01"),
-                  descripcion: "Día del Trabajador"
-                },
-                {
-                  calendario_dia_festivo: 4,
-                  calendario_escolar_id: 1,
-                  dia_festivo: new Date("2025-09-18"),
-                  descripcion: "Fiestas Patrias"
-                },
-                {
-                  calendario_dia_festivo: 5,
-                  calendario_escolar_id: 1,
-                  dia_festivo: new Date("2025-12-25"),
-                  descripcion: "Navidad"
-                }
-              ];
-            res.json(diasFestivos);              
-        } catch (error) {
-            res.status(500).json(error);
-        }
-    },
-    async actualizar (req: Request, res: Response) {
-        try {
-            const id = parseInt(req.params.id);
-            const calendarioActualizado = req.body;
-            const resultado = await dataService.updateById(id, calendarioActualizado);
-            res.status(200).json(resultado);
-        } catch (error) {
-            res.status(500).json(error);
-        }
-    },
-
-    async guardar(req: Request, res: Response) {
-        try {
-            const nuevoCalendario = req.body;
-            const resultado = await dataService.processData(nuevoCalendario);
-            res.status(201).json(resultado);
-        } catch (error) {
-            res.status(500).json(error);
-        }
-    },
-    async eliminar(req: Request, res: Response) {
-        try {
-            const id = parseInt(req.params.id);
-            const resultado = await dataService.deleteById(id);
-            res.status(200).json(resultado);
-        } catch (error) {
-            res.status(500).json(error);
-        }
+  async obtener(req: Request, res: Response) {
+    try {
+      const { colegio_id, ...where } = req.query;
+      let respuestaEnviada = false;
+      if (colegio_id !== undefined) {
+        const diasFestivos = await obtenerRelacionados({
+          tableFilter: "calendarios_escolares",
+          filterField: "colegio_id",
+          filterValue: colegio_id,
+          idField: "calendario_escolar_id",
+          tableIn: "calendarios_dias_festivos",
+          inField: "calendario_escolar_id",
+          selectFields: `*,                      
+                        calendarios_escolares(calendario_escolar_id,ano_escolar,fecha_inicio,fecha_fin,dias_habiles,colegios(colegio_id,nombre))`,
+        });
+        respuestaEnviada = true;
+        res.json(diasFestivos);
+      }
+      if (!respuestaEnviada) {
+        const diasFestivos = await dataService.getAll(
+          [
+            "*",
+            "calendarios_escolares(calendario_escolar_id,ano_escolar,fecha_inicio,fecha_fin,dias_habiles,colegios(colegio_id,nombre))",
+          ],
+          where
+        );
+        res.json(diasFestivos);
+      }
+    } catch (error) {
+      res.status(500).json(error);
     }
+  },
+  async actualizar(req: Request, res: Response) {
+    try {
+      const id = parseInt(req.params.id);
+      const calendarioActualizado = req.body;
+      const resultado = await dataService.updateById(id, calendarioActualizado);
+      res.status(200).json(resultado);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  },
 
-}
+  async guardar(req: Request, res: Response) {
+    try {
+      const nuevoCalendario = req.body;
+      const resultado = await dataService.processData(nuevoCalendario);
+      res.status(201).json(resultado);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  },
+  async eliminar(req: Request, res: Response) {
+    try {
+      const id = parseInt(req.params.id);
+      const resultado = await dataService.deleteById(id);
+      res.status(200).json(resultado);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  },
+};
