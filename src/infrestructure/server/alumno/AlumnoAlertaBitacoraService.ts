29a6a75d2b6e452241782182583240ac1121164c
henvisi-1994 <henvisi1994@gmail.com>
2025-05-30 15:05:41 -0500

    Correccion de validaciones

--- src/infrestructure/server/alumno/AlumnoAlertaBitacoraService.ts
+++ src/infrestructure/server/alumno/AlumnoAlertaBitacoraService.ts
@@ -1,10 +1,14 @@
 import { Request, Response } from "express";
 import { DataService } from "../DataService";
 import { AlumnoAlertaBitacora } from "../../../core/modelo/alumno/AlumnoAlertaBitacora";
+import { SupabaseClientService } from "../../../core/services/supabaseClient";
+import { SupabaseClient } from "@supabase/supabase-js";
 
 const dataService: DataService<AlumnoAlertaBitacora> = new DataService(
   "alumnos_alertas_bitacoras"
 );
+const supabaseService = new SupabaseClientService();
+const client: SupabaseClient = supabaseService.getClient();
 export const AlumnoAlertaBitacoraService = {
   async obtener(req: Request, res: Response) {
     try {
@@ -21,6 +25,21 @@ export const AlumnoAlertaBitacoraService = {
       const alumnoAlertaBitacora: AlumnoAlertaBitacora =
         new AlumnoAlertaBitacora();
       Object.assign(alumnoAlertaBitacora, req.body);
+        const { data, error } = await client
+        .from("alumnos")
+        .select("*")
+        .eq("alumno_id", alumnoAlertaBitacora.alumno_id)
+        .single();
+      if (error || !data) {
+        throw new Error("El alumno no existe");
+      }        const { data:dataAlumnoAlerta, error:errorAlumnoAlerta } = await client
+        .from("alumnos_alertas")
+        .select("*")
+        .eq("alumno_alerta_id", alumnoAlertaBitacora.alumno_alerta_id)
+        .single();
+      if (errorAlumnoAlerta || !dataAlumnoAlerta) {
+        throw new Error("La Alerta no existe");
+      }
       alumnoAlertaBitacora.creado_por = req.creado_por;
       alumnoAlertaBitacora.actualizado_por = req.actualizado_por;
       const savedAlumnoAlertaBitacora = await dataService.processData(
@@ -28,8 +47,7 @@ export const AlumnoAlertaBitacoraService = {
       );
       res.status(201).json(savedAlumnoAlertaBitacora);
     } catch (error) {
-      console.error("Error al guardar la alerta:", error);
-      res.status(500).json({ message: "Error interno del servidor" });
+     res.status(500).json({ message: (error as Error).message });
     }
   },
   async actualizar(req: Request, res: Response) {
@@ -45,8 +63,8 @@ export const AlumnoAlertaBitacoraService = {
         .status(200)
         .json({ message: "Alumno Alerta Bitácora actualizada correctamente" });
     } catch (error) {
-      console.error("Error al actualizar la alerta:", error);
-      res.status(500).json({ message: "Error interno del servidor" });
+     res.status(500).json({ message: (error as Error).message });
+
     }
   },
   async eliminar(req: Request, res: Response) {
