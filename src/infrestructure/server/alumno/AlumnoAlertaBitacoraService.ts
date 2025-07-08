a12d4daef86c56d594826e05fcf7bdbf67fdc8f3
SpiritWhite <juliyca.22@gmail.com>
2025-07-07 21:17:05 -0600

    fix: Alerta estado, agregacion de colegio_id

--- src/infrestructure/server/alumno/AlumnoAlertaBitacoraService.ts
+++ src/infrestructure/server/alumno/AlumnoAlertaBitacoraService.ts
@@ -5,6 +5,8 @@ import { AlumnoAlertaBitacora } from "../../../core/modelo/alumno/AlumnoAlertaBi
 import { SupabaseClientService } from "../../../core/services/supabaseClient";
 import { SupabaseClient } from "@supabase/supabase-js";
 import Joi from "joi";
+import { extractBase64Info, getExtensionFromMime, getURL, isBase64DataUrl } from "../../../core/services/ImagenServiceCasoUso";
+import { randomUUID } from "crypto";
 
 const dataService: DataService<AlumnoAlertaBitacora> = new DataService(
   "alumnos_alertas_bitacoras",
@@ -110,6 +112,27 @@ guardar: async (req: Request, res: Response) => {
     }
     alumnoAlertaBitacora.creado_por = req.creado_por;
     alumnoAlertaBitacora.actualizado_por = req.actualizado_por;
+
+    if (isBase64DataUrl(alumnoAlertaBitacora.url_archivo || " ")) {
+      const { mimeType, base64Data } = extractBase64Info(
+        alumnoAlertaBitacora.url_archivo || " "
+      );
+      const buffer = Buffer.from(base64Data, "base64");
+      const extension = getExtensionFromMime(mimeType);
+      const fileName = `${randomUUID()}.${extension}`;
+      const client_file = req.supabase;
+
+      const { error } = await client_file.storage
+        .from("bitacoras")
+        .upload(`documents/${fileName}`, buffer, {
+          contentType: mimeType,
+          upsert: true,
+        });
+      if (error) throw error;
+      alumnoAlertaBitacora.url_archivo = getURL(client_file, 'bitacoras', `documents/${fileName}`);
+    }
+
+
     // Guarda la bitácora
     const saved = await dataService.processData(alumnoAlertaBitacora);
     res.status(201).json(saved);
