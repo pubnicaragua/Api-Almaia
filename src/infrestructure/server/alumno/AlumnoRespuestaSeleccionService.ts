5d3266107d6ad70cd90c0d441b1711fa63c0cb07
SpiritWhite <juliyca.22@gmail.com>
2025-07-15 10:29:19 -0600

    feature: Nuevo endpoint para cambiar estado y modificacion de la consulta de las preguntas del dia

--- src/infrestructure/server/alumno/AlumnoRespuestaSeleccionService.ts
+++ src/infrestructure/server/alumno/AlumnoRespuestaSeleccionService.ts
@@ -25,6 +25,12 @@ const RespuestaSchema = Joi.object({
   id_registro: Joi.number().integer().required(),
   respuestas_posibles: Joi.array().items(Joi.object<{ respuesta_posible_id: number }>()).min(1).optional(),
   respuesta_posible_id: Joi.number().integer().optional(),
+  respuesta_posible_txt: Joi.string().min(1).optional(),
+});
+const CambiarEstadoSchema = Joi.object({
+  tipo_pregunta_id: Joi.number().integer().required(),
+  id_registro: Joi.number().integer().required(),
+  estado: Joi.boolean().required(),
 });
 
 const supabaseService = new SupabaseClientService();
@@ -32,49 +38,15 @@ const client: SupabaseClient = supabaseService.getClient();
 export const AlumnoRespuestaSeleccionService = {
   async obtener(req: Request, res: Response) {
     try {
-      const { colegio_id, tipo_pregunta_id = 1, ...where } = req.query;
+      const {
+        colegio_id, //NO SE USA, DEJA POR COMO TRABAJA EL FRONT
+        tipo_pregunta_id = 1,
+        respondio = false,
+        fecha = moment().format('YYYY-MM-DD'),
+        ...where } = req.query;
 
-      // const finalWhere = {
-      //   ...where,
-      //   activo: true,
-      //   respondio: false,
-      //   'fecha_creacion::date': moment().format('YYYY-MM-DD'),
-      // };
 
-      // let respuestaEnviada = false;
-
-      // if (colegio_id !== undefined) {
-      //   const alumnos_cursos = await obtenerRelacionados({
-      //     tableFilter: "alumnos",
-      //     filterField: "colegio_id",
-      //     filterValue: colegio_id,
-      //     idField: "alumno_id",
-      //     tableIn: "alumnos_respuestas_seleccion",
-      //     inField: "alumno_id",
-      //     selectFields: `*,
-      //                     alumnos(alumno_id,url_foto_perfil,telefono_contacto1,telefono_contacto2,email)
-      //                     preguntas(pregunta_id,texto_pregunta,grupo_preguntas,tipo_pregunta_id,nivel_educativo_id,template_code,respuestas_posibles(respuesta_posible_id,nombre,icono)),
-      //                     respuestas_posibles(respuesta_posible_id,nombre)`,
-      //   });
-      //   respuestaEnviada = true;
-      //   res.json(alumnos_cursos);
-      // }
-      // if (!respuestaEnviada) {
-      //   const alumnoRespuestaSeleccion = await dataService.getAll(
-      //     [
-      //       "*",
-      //       "alumnos(alumno_id,url_foto_perfil,telefono_contacto1,telefono_contacto2,email)",
-      //       "preguntas(pregunta_id,texto_pregunta,grupo_preguntas,tipo_pregunta_id,nivel_educativo_id,template_code,respuestas_posibles(respuesta_posible_id,nombre,icono))",
-      //       "respuestas_posibles(respuesta_posible_id,nombre)",
-      //     ],
-      //     finalWhere
-      //   );
-
-      //   res.json(alumnoRespuestaSeleccion);
-      // }
-
-      if (Number(tipo_pregunta_id) !== 3) {
-        let query = client
+      let query = client
         .from('alumnos_respuestas_seleccion')
         .select(
           [
@@ -85,25 +57,47 @@ export const AlumnoRespuestaSeleccionService = {
           ].join(',')
         )
         .eq('activo', true)
-        .eq('respondio', false)
-        .gte('fecha_creacion::date', moment().format('YYYY-MM-DD'))
+        .eq('respondio', respondio)
+        .gte('fecha_creacion::date', fecha)
         .order('fecha_creacion', { ascending: true });
 
       Object.keys(where).forEach((key) => {
         query = query.eq(key, where[key]);
-      });  
+      });
 
       const { data, error } = await query.returns<any[]>();
 
       if (error) {
-        // console.error("Error al obtener las respuestas de los alumnos:", error);
         throw error;
       }
 
-      res.status(200).json(data);
-      } else {
-        res.status(400).json({ message: "Tipo de pregunta no soportado para esta operación." });
+      let query2 = client
+        .from('alumnos_respuestas')
+        .select(
+          [
+            "*",
+            "alumnos(alumno_id,url_foto_perfil,telefono_contacto1,telefono_contacto2,email)",
+            "preguntas(pregunta_id,texto_pregunta,horario,grupo_preguntas,tipo_pregunta_id,nivel_educativo_id,template_code)",
+          ].join(',')
+        )
+        .eq('activo', true)
+        .eq('respondio', respondio)
+        .gte('fecha_creacion::date', fecha)
+        .order('fecha_creacion', { ascending: true });
+
+      Object.keys(where).forEach((key) => {
+        query2 = query2.eq(key, where[key]);
+      });
+
+      const { data: pabiertas, error: paerror } = await query2.returns<any[]>();
+
+      if (paerror) {
+        throw paerror;
       }
+
+      const preguntas_listado = [...data, ...pabiertas];
+
+      res.status(200).json(preguntas_listado);
       
     } catch (error) {
       console.error("Error al obtener el curso del alumno:", error);
@@ -477,11 +471,10 @@ export const AlumnoRespuestaSeleccionService = {
     }
   },
 
-
-async cambiarEstadoRespuesta(req: Request, res: Response) {    
+  async cambiarEstadoRespuesta(req: Request, res: Response) {    
   try {    
     const { alumno_id, pregunta_id, nuevo_estado, fecha } = req.body;    
-    console.log('cambiar estado');  
+    // console.log('cambiar estado');  
     
     if (!alumno_id || !pregunta_id || typeof nuevo_estado !== 'boolean') {    
       throw new Error("Faltan datos obligatorios o el estado no es válido.");    
@@ -554,6 +547,73 @@ async cambiarEstadoRespuestaMultiple(req: Request, res: Response) {
       message: error instanceof Error ? error.message : "Error interno del servidor"     
     });    
   }    
-}
+},
+
+  async omniCambiarEstadoPreguntas(req: Request, res: Response) {    
+  try {    
+    const { 
+      tipo_pregunta_id,
+      id_registro, 
+      nuevo_estado
+     } = req.body;    
+
+    const { error: validationError } = CambiarEstadoSchema.validate({
+      tipo_pregunta_id,
+      id_registro,
+      estado: nuevo_estado
+    }, { abortEarly: false });
+
+    if (Array.isArray(validationError?.details) && validationError?.details.length > 0)
+      res.status(400).json({
+        status: "error",
+        details: validationError.details.map(detail => detail.message)
+      });
+
+    if (!id_registro) {
+      res.status(400).json({ message: "Falta el ID del registro." });
+      return;
+    }
+
+    if (tipo_pregunta_id !== 3) {
+      const { error } = await client
+        .from("alumnos_respuestas_seleccion")
+        .update({
+          respondio: nuevo_estado,
+          fecha_actualizacion: req.fecha_creacion || new Date()
+        })
+        .match({
+          alumno_respuesta_seleccion_id: id_registro
+        });
+
+      if (error) {
+        throw new Error(error.message);
+      }
+    } else {
+      const { error } = await client
+        .from("alumnos_respuestas")
+        .update({
+          respondio: nuevo_estado,
+          fecha_actualizacion: req.fecha_creacion || new Date()
+        })
+        .match({
+          alumno_respuesta: id_registro
+        });
+
+      if (error) {
+        throw new Error(error.message);
+      }
+    }  
+    
+    res.status(201).json({     
+      message: `Estado de respuesta cambiado a ${nuevo_estado ? 'respondido' : 'no respondido'} correctamente.`     
+    });    
+    
+  } catch (error) {    
+    console.error("Error al cambiar estado de respuesta:", error);    
+    res.status(500).json({     
+      message: error instanceof Error ? error.message : "Error interno del servidor"     
+    });    
+  }    
+},
 
 };
