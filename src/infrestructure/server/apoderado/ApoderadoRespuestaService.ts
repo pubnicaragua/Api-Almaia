1d23c5dfcc5529088411a9c7d83eada1f6f1563d
SpiritWhite <juliyca.22@gmail.com>
2025-07-16 15:16:33 -0600

    fix: Agregar campos requeridos en preguntas en apoderados

--- src/infrestructure/server/apoderado/ApoderadoRespuestaService.ts
+++ src/infrestructure/server/apoderado/ApoderadoRespuestaService.ts
@@ -45,7 +45,7 @@ export const ApoderadoRespuestaService = {
           [
             "*",
             "alumnos(alumno_id,url_foto_perfil,personas(persona_id,nombres,apellidos))",
-            "preguntas(pregunta_id,texto_pregunta,respuestas_posibles(respuesta_posible_id,nombre))",
+            "preguntas(pregunta_id,texto_pregunta,horario,grupo_preguntas,tipo_pregunta_id,template_code,respuestas_posibles(respuesta_posible_id,nombre))",
             "apoderados(apoderado_id,personas(persona_id,nombres,apellidos),telefono_contacto1,telefono_contacto2,email_contacto1,email_contacto2)",
             "respuestas_posibles(respuesta_posible_id,nombre)",
           ].join(',')
