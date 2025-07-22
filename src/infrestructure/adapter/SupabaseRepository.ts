c13abba812dea7f97b80dd1a6dd08a8355ec038c
DavidCastro1229 <davidcastro505personal@gmail.com>
2025-07-22 10:50:06 -0600

    mejora de filtros por query

--- src/infrestructure/adapter/SupabaseRepository.ts
+++ src/infrestructure/adapter/SupabaseRepository.ts
@@ -6,16 +6,16 @@ import { SupabaseClientService } from "../../core/services/supabaseClient";
 export class SupabaseRepository<T> implements ISupabaseRepository<T> {
   private client!: SupabaseClient;
   private table: string = "";
-      protected pkName:string;
+  protected pkName: string;
 
-  constructor(tableName: string,pkName:string="id") {
+  constructor(tableName: string, pkName: string = "id") {
     this.table = tableName;
     this.pkName = pkName
     this.init().catch((err) => {
       throw new Error(`Error al inicializar SupabaseClient: ${err.message}`);
     });
   }
- setClient(client: SupabaseClient) {
+  setClient(client: SupabaseClient) {
     this.client = client;
   }
 
@@ -39,41 +39,46 @@ export class SupabaseRepository<T> implements ISupabaseRepository<T> {
     if (error) throw new Error(error.message);
     return data[0];
   }
-async getAll<T>(  
-  columns: string[],  
-  where: Record<string, any> = {},  
-  orderBy?: string, // columna opcional para ordenar  
-  ascending: boolean = true // dirección del ordenamiento, por defecto ascendente  
-): Promise<T[]> {  
-  await this.ensureClientInitialized();  
-  
-  if (!columns.length) {  
-    throw new Error(  
-      "Debes especificar al menos una columna para generar el reporte."  
-    );  
-  }  
-  
-  const columnQuery = columns.join(",");  
-  let query = this.client.from(this.table).select(columnQuery);  
-  
-  Object.keys(where).forEach((key) => {  
-    query = query.eq(key, where[key]);  
-  });  
-  
-  if (orderBy && orderBy !== "*") {  
-    query = query.order(orderBy, { ascending: ascending });  
-  }  
-  
-  const { data, error } = await query.returns<T[]>();  
-  
-  if (error) {  
-    throw new Error(  
-      `Error al consultar la tabla '${this.table}': ${error.message}`  
-    );  
-  }  
-  
-  return data ?? [];  
-}
+  async getAll<T>(
+    columns: string[],
+    where: Record<string, any> = {},
+    orderBy?: string, // columna opcional para ordenar  
+    ascending: boolean = true // dirección del ordenamiento, por defecto ascendente  
+  ): Promise<T[]> {
+    await this.ensureClientInitialized();
+
+    if (!columns.length) {
+      throw new Error(
+        "Debes especificar al menos una columna para generar el reporte."
+      );
+    }
+
+    const columnQuery = columns.join(",");
+
+    let query = this.client.from(this.table).select(columnQuery);
+    Object.keys(where).forEach((key) => {
+      //si la query es un array, usar in
+      if (Array.isArray(where[key])) {
+        query = query.in(key, where[key]);
+      } else {
+        query = query.eq(key, where[key]);
+      }
+    });
+
+    if (orderBy && orderBy !== "*") {
+      query = query.order(orderBy, { ascending: ascending });
+    }
+
+    const { data, error } = await query.returns<T[]>();
+
+    if (error) {
+      throw new Error(
+        `Error al consultar la tabla '${this.table}': ${error.message}`
+      );
+    }
+
+    return data ?? [];
+  }
 
   async getData(id: number): Promise<any> {
     await this.ensureClientInitialized();
@@ -88,7 +93,7 @@ async getAll<T>(
   async deleteData(id: number): Promise<any> {
     await this.ensureClientInitialized();
     const { error } = await this.client.from(this.table)
-      .update({activo:false})
+      .update({ activo: false })
       .eq(this.pkName, id);
     if (error) throw new Error(error.message);
   }
@@ -96,11 +101,11 @@ async getAll<T>(
   async updateData(id: number, data: T): Promise<any> {
     await this.ensureClientInitialized();
 
-    const { data:dataUpdate,error } = await this.client
+    const { data: dataUpdate, error } = await this.client
       .from(this.table)
       .update(data)
       .eq(this.pkName, id);
-      if (error) throw new Error(error.message);
-      return dataUpdate
+    if (error) throw new Error(error.message);
+    return dataUpdate
   }
 }
