5de2c4639b009053891288fcc64fc23fec37be63
DavidCastro1229 <davidcastro505personal@gmail.com>
2025-08-08 18:06:37 -0600

    contacto

--- src/infrestructure/server/contacto/CONTACTO.SERVICE.ts
+++ src/infrestructure/server/contacto/CONTACTO.SERVICE.ts
@@ -1,19 +1,13 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { Request, Response } from "express";
-import { DataService } from "../DataService";
-import { Docente } from "../../../core/modelo/colegio/Docente";
-import { SupabaseClientService } from "../../../core/services/supabaseClient";
-import { SupabaseClient } from "@supabase/supabase-js";
 import Joi from "joi";
 import { EmailService } from "../../../core/services/EmailService";
-
-const supabaseService = new SupabaseClientService();
-const client: SupabaseClient = supabaseService.getClient();
 
 const ContactoAlmaiachema = Joi.object({
     nombre: Joi.string().max(100).required(),
     email: Joi.string().max(100).required(),
     telefono: Joi.string().max(50).required(),
+    to: Joi.string().max(100)
 });
 export const CONTACTO_SERVICES = {
     async contactoAlmaia(req: Request, res: Response) {
@@ -22,22 +16,25 @@ export const CONTACTO_SERVICES = {
             if (error) {
                 throw new Error("Campos requeridos (nombre, telefono, email)");
             }
-            const { nombre, telefono, email } = req.body
-            new EmailService().enviarEmail(["dxgabalt@gmail.com", "alexmedel@almaia.cl", "contacto@almaia.cl", "crivas@almaia.cl"], {
-                subject: "Nuevo contacto desde el sitio web AlmaIA",
-                html: `
+            console.log('entra ==============>', req.body)
+            const { nombre, telefono, email, to } = req.body
+            new EmailService().enviarEmail(
+                to ? [to] : ["dxgabalt@gmail.com", "alexmedel@almaia.cl", "contacto@almaia.cl", "crivas@almaia.cl"],
+                {
+                    subject: "Nuevo contacto desde el sitio web AlmaIA",
+                    html: `
                  <h2>¡🔐 CONTACTO RECIBIDO!</h2>
-        <p>Hemos recibido un nuevo contacto 📲</p>
-        <b>🧾 Datos del contacto:</b>
-        <ul>
-          <li>📛 Nombre: {{nombre}}</li>
-          <li>📧 Correo electronico: {{email}}</li>
-          <li>📱 Telefono: {{telefono}}</li> 
-          <li>🗓️ Fecha: {{fecha}}</li>
-          <link href="https://nextjs.org/"/>
-        </ul>
+                <p>Hemos recibido un nuevo contacto 📲</p>
+                <b>🧾 Datos del contacto:</b>
+                <ul>
+                <li>📛 Nombre: {{nombre}}</li>
+                <li>📧 Correo electronico: {{email}}</li>
+                <li>📱 Telefono: {{telefono}}</li> 
+                <li>🗓️ Fecha: {{fecha}}</li>
+                <link href="https://nextjs.org/"/>
+                </ul>
                 `
-            }, { nombre, telefono, email, fecha: new Date().toLocaleDateString() })
+                }, { nombre, telefono, email, fecha: new Date().toLocaleDateString() })
             res.json({ message: "Contacto enviado" });
         } catch (error: any) {
             res.status(400).json({ message: error.message });
