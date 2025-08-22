eddd2785464f738d96ec96fefba8d03d4050885e
bobopomo23 <joselupo329@gmail.com>
2025-08-21 23:17:08 -0600

    Arreglando bugs

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,18 @@
-module.exports = { // eslint-disable-line no-undef
+module.exports = {
   testEnvironment: 'node',
   collectCoverage: true,
   coverageDirectory: 'coverage',
   testMatch: ['**/tests/**/*.test.ts'],
   transform: {
-    '^.+\\.ts?$': 'ts-jest',
+    '^.+\\.ts$': 'ts-jest',
   },
-};
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.json',
+    },
+  },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  // setupFilesAfterEnv: ['<rootDir>/tests/setup.ts'], // Se comenta o elimina si no se usa el archivo
+}
